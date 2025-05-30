d8a31fd50ce6110df9e66e07b81bfc2ceadb22ac
Fatma çelebi <fatmakaygisiz97@gmail.com>
2025-05-30 10:21:54 +0300

    version1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        car car1 = new car();
+
+        car1.speed= 180;
+        System.out.println(car1.speed);
+    }
+}
